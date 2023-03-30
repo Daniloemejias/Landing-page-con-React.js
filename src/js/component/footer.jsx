8ec3ae3b6b00ab6b61be69b87271ff2d871d91b5
Daniloemejias <daniloemejias@gmail.com>
2023-03-30 02:52:08 +0000

    Landing Page with react.js

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () =>{
+    return (<nav className="navbar fixed-bottom navbar navbar-dark bg-dark py-0 my-0">
+        <div className="container-fluid bg-dark">
+            <a className="navbar-brand text-white" href="#">Sticky bottom</a>
+        </div>
+    </nav>)
+}
+
+export default Footer
